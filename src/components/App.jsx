9af9c5350faa5908e562418dfb36b77407577c5e
Dubyshkina <ek.kalyazina@gmail.com>
2023-05-07 22:08:14 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
+        fontWeight: 400,
         color: '#010101'
       }}
     >
